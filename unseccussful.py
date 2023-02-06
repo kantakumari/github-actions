ed98237668b2e38fe1ef6fa6ba55f62639e36a1a
kantakumari <kanta.kumari@publicissapient.com>
2023-02-06 13:56:53 +0530

    unsuccessful notification

--- unseccussful.py
+++ unseccussful.py
@@ -6,9 +6,8 @@ port = 465
 smtp_server = "smtp.gmail.com"
 USERNAME = os.environ.get('USER_EMAIL')
 PASSWORD = os.environ.get('USER_PASSWORD')
-SENDERNAME =os.environ.get('DEST')
 message = """\
-Subject: Github Actions failed !!
+Subject: Github Actions Failure
 
 Your job had failed !!!
 """
@@ -16,4 +15,4 @@ Your job had failed !!!
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(USERNAME,PASSWORD)
-    server.sendmail(USERNAME,SENDERNAME,message)
+    server.sendmail(USERNAME,USERNAME,message)
